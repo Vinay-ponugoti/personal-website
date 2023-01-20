d5df5080f024fd7cdd8c5d3e77d7c14a9d8af1f1
Vinay-ponugoti <121456719+Vinay-ponugoti@users.noreply.github.com>
2023-01-20 11:44:00 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 // Import Assets
 import plant from '../assets/plant.jpg';
+import stack from '../assets/Stack.png';
 
 
 const Projects = () => {
@@ -8,7 +9,15 @@ const Projects = () => {
             <h2>My Projects</h2>
 
             <div className="projects__cards">
-
+                <div className="projects__card">
+                    <h3><strong>Stackoverflow-Clone</strong></h3>
+                    <img src={stack} alt="stack-overflow" />
+                    <p>Stack Overflow is a question and answer website for programmers to share their knowledge and expertise with others.
+                         Users can ask and answer technical questions, share code snippets, and collaborate on solutions to programming problems. 
+                         </p>
+                    <a href="" target="_blank" className="button">Code</a>
+                    {/* <a className="button">Result</a> */}
+                </div>
                 <div className="projects__card">
                     <h3><strong>Plant Disease Detection</strong></h3>
                     <img src={plant} alt="plant" />

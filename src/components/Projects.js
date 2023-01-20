d7d4f1322c3d37224e94f5a886439b1161090e9a
Vinay-ponugoti <121456719+Vinay-ponugoti@users.noreply.github.com>
2023-01-20 11:51:56 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -23,7 +23,7 @@ const Projects = () => {
                     <img src={plant} alt="plant" />
                     <p>Plant disease detection using machine learning involves training a model on a dataset of labeled plant images,
                          where the labels indicate the presence or absence of a particular disease. The model can then be used to predict
-                          the likelihood of a disease being present in a new, unseen plant image.</p>
+                          the likelihood of a disease.</p>
 
                     <a href="https://github.com/Vinay-ponugoti/Plant-Disease-Detection" target="_blank" className="button">Code</a>
                     {/* <a className="button">Result</a> */}

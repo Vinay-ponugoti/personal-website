6371c0a6cb1f94bfe50079537dc9bfc7d09ab44a
Vinay-ponugoti <121456719+Vinay-ponugoti@users.noreply.github.com>
2024-02-13 13:40:36 -0500

    project-update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -18,6 +18,18 @@ const Projects = () => {
                     <a href="https://github.com/Vinay-ponugoti/Plant-Disease-Detection" target="_blank" className="button">Code</a>
                     {/* <a className="button">Result</a> */}
                 </div>
+
+                <div className="projects__cards">
+                <div className="projects__card">
+                    <h3><strong>Decentralised-voting-app</strong></h3>
+                    <img src={stack} alt="stack-overflow" />
+                    <p>Stack Overflow is a question and answer website for programmers to share their knowledge and expertise with others.
+                         Users can ask and answer technical questions, share code snippets, and collaborate on solutions to programming problems. 
+                         </p>
+                    <a href="https://github.com/Vinay-ponugoti/Decentralised-voting-app.git" target="_blank" className="button">Code</a>
+                    {/* <a className="button">Result</a> */}
+                </div>
+
                 <div className="projects__card">
                     <h3><strong>Plant Disease Detection</strong></h3>
                     <img src={plant} alt="plant" />

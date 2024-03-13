dbc5a2413bcd5be757fcaea9ab9ef88dcc62b570
Vinay-ponugoti <121456719+Vinay-ponugoti@users.noreply.github.com>
2024-03-13 13:25:36 -0400

    project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,11 +10,24 @@ const Projects = () => {
             <h2>My Projects</h2>
 
             <div className="projects__cards">
+
+            <div className="projects__card">
+                    <h3><strong>Plant Disease Detection</strong></h3>
+                    <img src={plant} alt="plant" />
+                    <p>Plant disease detection using machine learning involves training a model on a dataset of labeled plant images,
+                         where the labels indicate the presence or absence of a particular disease. The model can then be used to predict
+                          the likelihood of a disease.</p>
+
+                    <a href="https://github.com/Vinay-ponugoti/Plant-Disease-Detection" target="_blank" className="button">Code</a>
+                    {/* <a className="button">Result</a> */}
+                </div>
+
+
                 <div className="projects__card">
                     <h3><strong>Stackoverflow-Clone</strong></h3>
                     <img src={stack} alt="stack-overflow" />
-                    <p>Stack Overflow is a question and answer website for programmers to share their knowledge and expertise with others.
-                         Users can ask and answer technical questions, share code snippets, and collaborate on solutions to programming problems. 
+                    <p>DApp voting app: Redefining decision-making through transparent, secure, and collaborative voting processes. 
+                        Empower users to actively engage in decentralized governance, ensuring impactful and transparent outcomes for all participants. 
                          </p>
                     <a href="https://github.com/Vinay-ponugoti/Plant-Disease-Detection" target="_blank" className="button">Code</a>
                     {/* <a className="button">Result</a> */}
@@ -30,16 +43,7 @@ const Projects = () => {
                     {/* <a className="button">Result</a> */}
                 </div>
 
-                <div className="projects__card">
-                    <h3><strong>Plant Disease Detection</strong></h3>
-                    <img src={plant} alt="plant" />
-                    <p>Plant disease detection using machine learning involves training a model on a dataset of labeled plant images,
-                         where the labels indicate the presence or absence of a particular disease. The model can then be used to predict
-                          the likelihood of a disease.</p>
-
-                    <a href="https://github.com/Vinay-ponugoti/Plant-Disease-Detection" target="_blank" className="button">Code</a>
-                    {/* <a className="button">Result</a> */}
-                </div>
+                
                 
             </div>
         </section>
